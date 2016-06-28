098808b5d2df3a86d40c487476f20eafb791169c
Katherine ChengLi <katchengli@gmail.com>
2016-06-28 16:05:21 -0700

    First querying

--- Init/__init__.py
+++ Init/__init__.py
@@ -1,12 +1,18 @@
+import json, io
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
+from pprint import pprint
 
+with io.open('../yelp_auth.secret') as auth_file:
+    creds = json.load(auth_file)
+    auth = Oauth1Authenticator(**creds)
+    client = Client(auth)
 
-auth = Oauth1Authenticator(
-    consumer_key=YOUR_CONSUMER_KEY,
-    consumer_secret=YOUR_CONSUMER_SECRET,
-    token=YOUR_TOKEN,
-    token_secret=YOUR_TOKEN_SECRET
-)
+params = {
+    'term': 'coffee',
+    'lang': 'en'
+}
 
-client = Client(auth)
+coffee_shops = client.search('San Francisco', **params)
+pprint(coffee_shops.businesses[0].name)
+
