009278d0df7f3d4078b49eeb6d8cb47990de07c1
Katherine ChengLi <katchengli@gmail.com>
2016-06-29 15:44:48 -0700

    modifying gitignore + first flask app

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ Created on Jun 28, 2016
 @author: Katherine
 '''
 from model.yelp_client import YelpClient
+from flask import Flask
+app = Flask(__name__)
 
 
 if __name__ == '__main__':
+    pass
+    #print(results)
+
+@app.route('/')
+def getCoffee():
     client = YelpClient("yelp_auth.secret")
     results = client.fetchBusinesses()
-    print(results)
+    return results
--- test/test_main.py
+++ test/test_main.py
@@ -3,10 +3,6 @@ Created on Jun 28, 2016
 
 @author: Katherine
 '''
-from model.yelp_client import YelpClient
-
 
 if __name__ == '__main__':
-    client = YelpClient()
-    results = client.fetchBusinesses()
-    print(results)
+    pass
