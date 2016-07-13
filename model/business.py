bc3e64c700821b08aa565bf5df82acb73f486a7f
Katherine ChengLi <katchengli@gmail.com>
2016-07-13 12:36:48 -0700

    adding methods and tests for business

--- model/business.py
+++ model/business.py
@@ -4,6 +4,8 @@ Created on Jul 3, 2016
 @author: Katherine
 '''
 
+import json
+
 class Business(object):
     '''
     classdocs
@@ -18,8 +20,19 @@ class Business(object):
         self.lattitude = latit
         
     @classmethod
-    def buildFromYelpObject(cls, yelpBusiness):
+    def buildFirstBusinessFromYelpObject(cls, yelpBusiness):
         business = cls(yelpBusiness.name, yelpBusiness.location.coordinate.longitude, yelpBusiness.location.coordinate.latitude)
         
-        return business.__dict__
+        return business
         
+    @classmethod
+    def returnJSONOfBusinesses(cls, listOfBusinesses):
+        
+        return json.dumps([business.__dict__ for business in listOfBusinesses])
+    
+    @classmethod
+    def buildBusinessesListFromYelpObject(cls, yelpBusinesses):
+        
+        listOfBusinesses = list(cls(yelpBusiness.name, yelpBusiness.location.coordinate.longitude, yelpBusiness.location.coordinate.latitude) for yelpBusiness in yelpBusinesses)
+        
+        return listOfBusinesses
--- test/model/test_business.py
+++ test/model/test_business.py
@@ -3,15 +3,34 @@ Created on Jul 4, 2016
 
 @author: Katherine
 '''
-import unittest
+import unittest, json
 from model.business import Business
 from model.yelp_client import YelpClient
 
 class TestBusiness(unittest.TestCase):
 
 
-    def test_build_from_yelp_obj(self):
+    def test_build__first_businesss_from_yelp_obj(self):
         yelpBusiness = YelpClient("../../yelp_auth.secret").fetchFirstBusiness()
-        business = Business.buildFromYelpObject(yelpBusiness)
-        print(business)
+        business = Business.buildFirstBusinessFromYelpObject(yelpBusiness)
+        business = business.__dict__
         assert (business["businessName"] == "Blue Bottle Coffee Co")
+    
+    def test_return_JSON_of_businesses(self):
+        business1 = Business("abc", "1", "2")
+        business2 = Business("def", "3", "4")
+        business3 = Business("ghi", "5", "6")
+        listOfBusinesses = [business1, business2, business3]
+        
+        jsonOfBusinesses = Business.returnJSONOfBusinesses(listOfBusinesses)
+        
+        listOfBusinesses = json.loads(jsonOfBusinesses)
+        
+        assert (listOfBusinesses[1]["businessName"] == "def")
+        
+    def test_build_businessses_list_from_yelp_obj(self):
+        yelpBusinesses = YelpClient("../../yelp_auth.secret").fetchCoffeeBusinesses()
+        listOfBusinesses = Business.buildBusinessesListFromYelpObject(yelpBusinesses)
+        
+        assert (listOfBusinesses[0].businessName == "Blue Bottle Coffee Co")
+        
